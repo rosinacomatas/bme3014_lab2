ae513d022cfddbea5ac06f6a25794aa52ae15f95
pbonarrigo <58232560+pbonarrigo@users.noreply.github.com>
2020-11-11 15:18:54 -0500

    Add files via upload

--- BME3014_lab2_group1.m
+++ BME3014_lab2_group1.m
@@ -21,7 +21,8 @@ zdir = fname(:,3);
 
 % Create a time array (use the Fs variable from above)
 time = [];
-time(1) = 1/Fs;
+time(1) = 0; 
+time(2) = 1/Fs;
 temptime = 1/Fs;
 for k = 2:length(zdir)
     time(k) = time(k-1) + temptime;
@@ -29,6 +30,9 @@ end
 % Subtract the users weight from the entire dataset <- This is the data you
 %   will use for the remainder of the analysis
 zforce = zdir-userwt;
+
+zforce(zforce == -userwt) = [];
+time = (1:length(zforce))
 
 
 %% Programmatically identify start and end of "air" time
@@ -52,6 +56,9 @@ air = find(threshdata == true);   %air time
 BeginningofAirtime = find(threshdata, 1, 'first')
 EndofAirtime = find(threshdata, 1, 'last')
 
+airtime = time(EndofAirtime) - time(BeginningofAirtime) 
+
+
 %% Programmatically identify the start of the jump impulse and end of landing impulse
 
 % Hint: the data before the jump impulse or after ther landing impulse is
@@ -59,26 +66,15 @@ EndofAirtime = find(threshdata, 1, 'last')
 %   start of the jump or end of the landing by finding where the data
 %   begins to deviate from zero
 
-level = 5;
+level = 150;
 
-threshdata = false(size(zforce(1:BeginningofAirtime))); % Create a thresholded array
-threshdata(zforce > level) = true;  %anything below the threshold is true
+threshdata = false(size(zforce)); % Create a thresholded array
+threshdata(zforce > level) = true;
+threshdata(zforce < -level) = true;%anything below the threshold is true
 
-BeginImpulse = find(threshdata, 1, 'first')
+BeginImpulse = find(threshdata(15:end), 1, 'first') + 15
 
-level = -5;
-
-threshdata = false(size(zforce(EndofAirtime:end))); % Create a thresholded array
-threshdata(zforce(EndofAirtime:end) < level) = true;  %anything below the threshold is true
-
-BeginLandingDown = find(threshdata, 1, 'first') + EndofAirtime%This may not work since this person never lands
-
-level = -2.5;
-
-threshdata = false(size(zforce(BeginLandingDown:end))); % Create a thresholded array
-threshdata(zforce > level) = true;  %anything below the threshold is true
-
-EndLandingDown = find(threshdata, 1, 'last') + BeginLandingDown
+EndImpulse = find(threshdata, 1, 'last')
 
 %% Calculate the value of the jumping impulse and landing impulse in N*s
 % "trapz" is a built in function that can be used to numerically integrate
@@ -86,21 +82,30 @@ EndLandingDown = find(threshdata, 1, 'last') + BeginLandingDown
 
 Jumpimpulse = trapz(zforce(BeginImpulse:BeginningofAirtime),time(BeginImpulse:BeginningofAirtime))
 
-%Landingimpulse = trapz(zforce(EndofAirtime:EndLandingDown),time(EndofAirtime:EndLandingDown))
+Landingimpulse = trapz(zforce(EndofAirtime:EndImpulse),time(EndofAirtime:EndImpulse))
 
 
 %% Calculate peak landing force
 
-maxlandingforce = max(zforce(EndofAirtime:EndLandingDown))
+maxlandingforce = max(zforce(EndofAirtime:EndImpulse))
+maxjumpforce = max(zforce(BeginImpulse:BeginningofAirtime))
+if maxlandingforce > maxjumpforce
+    maxforce = maxlandingforce
+else
+    maxforce = jumpforce
+end
 
 %% Use the air time to determine theoretical jump height
 % Based on conservation of momentum/projectile motion or energy equations.
 % The subject can be considered a "projectile" when in the air
 
-%airtime = time(EndofAirtime) - time(BeginningofAirtime) 
+airtime = time(EndofAirtime) - time(BeginningofAirtime) 
 vi = 0
 a = 9.81
-Jumpheight = vi*t +(at^2)/2
+
+airtime =airtime/200
+
+Jumpheight = vi*airtime +(a*(airtime)^2)/2
 
 
 
@@ -108,11 +113,11 @@ Jumpheight = vi*t +(at^2)/2
 % Use the form: disp(['MEASURED PARAMETER ',num2str(VALUE), ' UNITS'])
 % Report total jumping impulse and landing impulse
 
-%disp('JUMP IMPULSE ', Jumpimpulse, 'Ns ', 'LANDING IMPULSE ', Landingimpulse, 'Ns ')
+fprintf('JUMP IMPULSE ') disp(Jumpimpulse) fprintf('Ns ') fprintf('LANDING IMPULSE ') disp(Landingimpulse) fprintf('Ns ')
 
 % Report peak force
 
-%disp('PEAK FORCE ', maxlandingforce, 'N ')
+disp('PEAK FORCE ', maxlandingforce, 'N ')
 
 
 % Report estimated jump height
@@ -121,11 +126,11 @@ disp('ESTIMATED JUMP HEIGHT ', Jumpheight, 'm')
 
 % Report jumping impulse normalized to (divided by) jumping height
 
-%disp('NORMALIZED LANDING IMPULSE ', Jumpimpulse/Jumpheight, 'Ns ','NORMALIZED LANDING IMPULSE ', Landingimpulse/Jumpheight, 'Ns ')
+disp('NORMALIZED LANDING IMPULSE ', Jumpimpulse/Jumpheight, 'Ns ','NORMALIZED LANDING IMPULSE ', Landingimpulse/Jumpheight, 'Ns ')
 
 % Report peak force normalized to jump height
 
-%disp('PEAK FORCE NORMALIZED', maxlandingforce/Jumpheight, 'N ')
+disp('PEAK FORCE NORMALIZED', maxlandingforce/Jumpheight, 'N ')
 
 %% Plot data in the following way
 % Plot your data (the data with the user's weight removed) as a black line
